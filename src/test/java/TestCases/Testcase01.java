119b44ea5048e5d7add828b84d5de9b897e57f32
Omar Adel <omaradellg@gmail>
2023-05-12 15:07:08 +0300

    ADD NEW TEST CASE

---
+++ src/test/java/TestCases/Testcase01.java
@@ -0,0 +1,10 @@
+package TestCases;
+
+import org.example.Main;
+
+public class Testcase01 {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(x);
+    }
+}
